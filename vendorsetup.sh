e215e49a369b6f3f0d63185950ea98c8019a0a9f
Ghnkz <49773907+Ghnkz@users.noreply.github.com>
2025-11-11 09:07:36 +0700

    Create vendorsetup.sh to clone device dependencies
    Added a script to clone necessary dependencies for the Xiaomi sweet device.

---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+deviceDir=$(gettop)/device/xiaomi/sweet
+
+${deviceDir}/applypatch.sh ${deviceDir}/patches
+
+# Cloning dependencies
+git clone https://github.com/Ghnkz/device_xiaomi_sm6150-common -b arrow-13.1 device/xiaomi/sm6150-common
+
+git clone https://github.com/Ghnkz/vendor_xiaomi_sm6150-common -b arrow-13.1 vendor/xiaomi/sm6150-common
+
+git clone https://github.com/Ghnkz/vendor_xiaomi_sweet -b arrow-13.1 vendor/xiaomi/sweet
+
+git clone https://github.com/Ghnkz/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
+
+git clone https://github.com/Ghnkz/vendor_xiaomi_sweet-miuicamera vendor/xiaomi/sweet-miuicamera
+
+git clone https://github.com/Ghnkz/android_packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+rm -rf hardware/xiaomi && git clone https://github.com/PixelOS-AOSP/hardware_xiaomi -b thirteen hardware/xiaomi
