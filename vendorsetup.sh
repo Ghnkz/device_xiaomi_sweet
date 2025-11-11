abd7f66f3976f907e2176adfe62662b6f92f4507
Ghnkz <49773907+Ghnkz@users.noreply.github.com>
2025-11-12 06:47:40 +0700

    Update kernel repository branch in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-deviceDir=$(gettop)/device/xiaomi/sweet
-
-${deviceDir}/applypatch.sh ${deviceDir}/patches
-
 # Cloning dependencies
 git clone https://github.com/Ghnkz/device_xiaomi_sm6150-common -b arrow-13.1 device/xiaomi/sm6150-common
 
@@ -11,7 +7,7 @@ git clone https://github.com/Ghnkz/vendor_xiaomi_sm6150-common -b arrow-13.1 ven
 
 git clone https://github.com/Ghnkz/vendor_xiaomi_sweet -b arrow-13.1 vendor/xiaomi/sweet
 
-git clone https://github.com/Ghnkz/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
+git clone https://github.com/Ghnkz/kernel_xiaomi_sm6150 -b bka kernel/xiaomi/sm6150
 
 git clone https://github.com/Ghnkz/vendor_xiaomi_sweet-miuicamera vendor/xiaomi/sweet-miuicamera
 
